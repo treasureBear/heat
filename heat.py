fc8b92b53bc9d4a3df9f8ec7ad470c1583263615
treasureBear <bjorn.skatt@gmail.com>
2016-01-03 21:38:59 +0100

    TS writes all fields and read stub

--- heat.py
+++ heat.py
@@ -1,8 +1,21 @@
 #Heat - My boiler room automation project
 
-def ts_write():
+def ts_write(t_in,t_out,t_acc,t_room,t_outside,l_pellets,s_set):
     import urllib
     import urllib.request
-    urllib.request.urlopen("https://api.thingspeak.com/update?api_key=0VLIR53FNSJ1KWWW&field1=100")
+    url = "https://api.thingspeak.com/update?api_key=0VLIR53FNSJ1KWWW"
+    url = url+"&field1="+str(t_in)
+    url = url+"&field2="+str(t_out)
+    url = url+"&field3="+str(t_acc)
+    url = url+"&field4="+str(t_room)
+    url = url+"&field5="+str(t_outside)
+    url = url+"&field6="+str(l_pellets)
+    url = url+"&field7="+str(s_set)
+    urllib.request.urlopen(url)
 
-ts_write()
+    msg = urllib.request.urlopen("https://api.thingspeak.com/channels/76296/feeds/last.json")
+    return msg
+
+t_target = ts_write(2,1.1,1.2,1.3,1.4,1.5,1.6)
+print(t_target)
+print(t_target.read())
